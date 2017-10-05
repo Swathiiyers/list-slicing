3a915f70cfb1b8e10630fb607ef73bab8b956881
Hackbright Student <no-reply@hackbrightacademy.com>
2017-10-05 12:55:44 -0700

    working function 7

--- list_operations.py
+++ list_operations.py
@@ -97,7 +97,7 @@ def last_five(input_list):
 
     """
 
-    return []
+    return input_list[-5:]
 
 
 def middle(input_list):
@@ -110,7 +110,7 @@ def middle(input_list):
 
     """
 
-    return []
+    return input_list[2:-2]
 
 
 def inner_four(input_list):
